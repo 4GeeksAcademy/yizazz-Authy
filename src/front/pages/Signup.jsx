7381a5f6c6fbf4497c52d24bbb2ab2a73a5c3baf
yizazz <jesusastorga53@gmail.com>
2025-11-03 00:28:03 +0000

    login and signup views almost ready

---
+++ src/front/pages/Signup.jsx
@@ -0,0 +1,33 @@
+export const Signup = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return <>
+        <form className="form container mt-5">
+            <div className="mb-3">
+                <label htmlFor="exampleInputName" className="form-label">Name</label>
+                <input type="text" className="form-control" id="exampleInputName" aria-describedby="nameHelp" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputLastName" className="form-label">Last Name</label>
+                <input type="text" className="form-control" id="exampleInputLastName" aria-describedby="lastNameHelp" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+            <button 
+            type="submit" 
+            className="btn btn-primary"
+            onClick={handleSubmit}
+            >Submit</button>
+        </form>
+    </>
+}
